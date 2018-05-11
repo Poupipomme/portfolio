f0fbacf175b6aa3864b1066e1cef5f8197fc506d
Poupipomme <poupipomme@gmail.com>
2018-05-11 14:37:26 +0200

    new rep

---
+++ script/jquery.js
@@ -0,0 +1,32 @@
+$(document).ready(function(){
+    $('.parallax').parallax( {
+        responsiveThreshold: 993
+    });
+    $(".sb-container").scrollBox();
+    $("tooltipped").tooltip();
+}
+
+$("body").niceScroll();
+
+$('enter_link').click(function() {
+  $(this).parent().fadeOut(500);
+});
+
+$(function() {
+    /**
+    * Smooth scrolling to page anchor on click
+    **/
+    $("a[href*='#']:not([href='#'])").click(function() {
+        if (location.hostname == this.hostname && this.pathname.replace(/^\//,"") == location.pathname.replace(/^\//,""))
+        {
+            var anchor = $(this.hash);
+            anchor = anchor.length ? anchor : $("[name=" + this.hash.slice(1) +"]");
+            if ( anchor.length )
+            {
+                $("html, body").animate( {
+                  scrollTop: anchor.offset().top
+                }, 1500);
+            }
+        }
+    });
+});
